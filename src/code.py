50f6cd61496fbf24f5957c69cb2644f280583f3d
Seiki Tokunaga <otokuna46@gmail.com>
2022-08-06 12:17:22 +0900

    Fix test code

--- src/code.py
+++ src/code.py
@@ -1,16 +1,15 @@
 from os import remove
 from os.path import isfile
 import traceback
-
 ERROR_MESSAGE_FILE_NOT_EXIST='ファイルが存在しません。'
 ERROR_MESSAGE_FILE_DElETED='ファイルを削除しました。'
 ERROR_MESSAGE='エラーが発生しました。'
 def delete_file(file_path):
     try:
         if not isfile(file_path):
-            return ERROR_MESSAGE_FILE_DElETED
+            return ERROR_MESSAGE_FILE_NOT_EXIST
         remove(file_path)
         return ERROR_MESSAGE_FILE_DElETED
     except:
         print(traceback.format_exc())
-        return ERROR_MESSAGE_FILE_DElETED
+        return ERROR_MESSAGE
--- tests/test_delete.py
+++ tests/test_delete.py
@@ -1,21 +1,30 @@
 import os
 import src.code  as code
+import pytest
 
 
+@pytest.fixture(autouse=True)
+def init_mock(mocker):
+    #code内の関数をそれぞれmock化
+    #共通処理はfixtureでまとめて記載する
+    def isdelete_mock_func(file_path):
+        return True
+    mocker.patch('src.code.remove', isdelete_mock_func)
 
 def test_file_not_exist_with_mock_func(mocker):
     def isfile_mock_func(file_path):
         return False
-    #サイドエフェクトを利用
-    mocker.patch('os.path.isfile', side_effect=isfile_mock_func)
-    message = delete_file('test_file_path')
+    #code内の関数をそれぞれmock化
+    mocker.patch('src.code.isfile', isfile_mock_func)
+    message = code.delete_file('test_file_path')
     assert message == 'ファイルが存在しません。'
-def test_file_not_exist(mocker):
-    # os.path.isfile をモック化する(常にFalse)
-    mocker.patch('os.path.isfile', return_value=False)
-
+def test_failed_file_not_exist(mocker):
+    def isfile_mock_func(file_path):
+        return True
+    #code内の関数をそれぞれmock化
+    #常にTrue
+    mocker.patch('src.code.isfile', isfile_mock_func)
     # テスト対象を実行する
     message = code.delete_file('test_file_path')
-
     # 戻り値のメッセージを検証
-    assert message == 'ファイルが存在しません。'
+    assert message == 'ファイルを削除しました。'
